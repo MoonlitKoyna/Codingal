40b20e04dae9af6cb926d2efa9ba00cdf940be2d
MoonlitKoyna <koynauwu@gmail.com>
2026-03-15 15:17:50 +0530

    Add main.py for database type quiz functionality

---
+++ Guess Database/main.py
@@ -0,0 +1,10 @@
+print("What is the type of above given database - \n 1)Relational Database \n 2)Non-Relational Database")
+
+answer = int(input("Enter your answer here..."))
+
+if answer == 2:
+    print("Correct! The above given database is a Non-Relational Database.")
+else:
+    print("Incorrect! The above given database is a Non-Relational Database.")
+
+print("\n Please tell your mentor why you guessed this?")
