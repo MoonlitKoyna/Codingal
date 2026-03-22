d5b3f29c0dd9247656fe909f12bf6ab283359bc7
MoonlitKoyna <koynauwu@gmail.com>
2026-03-22 05:43:16 +0530

    Add main.py for LCM calculation functionality

---
+++ LCM/main.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+lcm = max(num1, num2)
+
+while True:
+    if lcm % num1 == 0 and lcm % num2 == 0:
+        print("LCM is:", lcm)
+        break
+    lcm += 1
